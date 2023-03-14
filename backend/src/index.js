e872fa05d73724233d3e50ce037e81666919d799
qbuus <jkocz867@gmail.com>
2023-03-14 13:26:53 +0100

    MongoDB setup and schema user

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(cors);
 
+mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+
 app.listen(SERVERPORT, () => {
   console.log(`server started on Port ${SERVERPORT}`);
 });
