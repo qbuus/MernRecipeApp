3568c6da415f1e1f456370fcc640de45f79414bd
qbuus <jkocz867@gmail.com>
2023-03-14 14:42:38 +0100

    initial schema and routes to test connection works correctly

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,16 +3,23 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users.js";
 
 const SERVERPORT = process.env.PORT || 3010;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
-mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+app.use("/auth", userRoutes);
 
-app.listen(SERVERPORT, () => {
-  console.log(`server started on Port ${SERVERPORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("mongoDB connected");
+    app.listen(SERVERPORT, () => {
+      console.log(`server started on Port ${SERVERPORT}`);
+    });
+  })
+  .catch(console.error);
