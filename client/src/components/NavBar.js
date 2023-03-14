65b9151cb1d6c1d79148ff56ab7dbd5d2dfe4ad2
qbuus <jkocz867@gmail.com>
2023-03-14 17:20:35 +0100

    Login and register functionality, storing jwt token

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const NavBar = () => {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.removeItem("userID");
+    navigate("/auth");
+  };
+
   return (
     <div className="navbar">
       <Link to="/">Home</Link>
       <Link to="/createRecipe">Create Recipe</Link>
       <Link to="/savedRecipes">Saved Recipe</Link>
-      <Link to="/auth">Login / Register</Link>
+      {!cookies.access_token ? (
+        <Link to="/auth">Login / Register</Link>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
     </div>
   );
 };
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,7 +1,116 @@
 import React from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <div className="auth">
+      <Login />
+      <Register />
+    </div>
+  );
+};
+
+const Login = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const [_, setCookies] = useCookies(["access_token"]);
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await axios.post(
+        "http://localhost:3001/auth/login",
+        { username, password }
+      );
+
+      setCookies("access_token", resp.data.token);
+      window.localStorage.setItem("userID", resp.data.userID);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      Label="Login"
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+const Register = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/auth/register", {
+        username,
+        password,
+      });
+      alert("Sign up done, Log in now");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      Label="Register"
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+const Form = ({
+  username,
+  setUsername,
+  password,
+  setPassword,
+  Label,
+  onSubmit,
+}) => {
+  return (
+    <div className="auth-container">
+      <form onSubmit={onSubmit}>
+        <h2>{Label}</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
