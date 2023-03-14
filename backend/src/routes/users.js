b83148f9a72ee2ea9093dc6fb380740ef68c035a
qbuus <jkocz867@gmail.com>
2023-03-14 14:49:51 +0100

    user register func

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -10,7 +10,16 @@ router.post("/register", async (req, res) => {
 
   const user = await UserModel.findOne({ username });
 
-  res.json(user);
+  if (user) {
+    return res.json({ message: "User already exists" });
+  }
+
+  const hashedPass = await bcrypt.hash(password, 10);
+
+  const newUser = new UserModel({ username, password: hashedPass });
+  await newUser.save();
+
+  res.json({ message: "User registered successfully" });
 });
 
 router.post("login");
