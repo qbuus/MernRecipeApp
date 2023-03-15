4467f5f680fb71064a41d83cc0b0c65679db61b2
qbuus <jkocz867@gmail.com>
2023-03-15 12:08:57 +0100

    Adding recipe page

--- backend/src/routes/recipes.js
+++ backend/src/routes/recipes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
@@ -18,6 +19,39 @@ router.post("/", async (req, res) => {
   try {
     const resp = await recipe.save();
     res.json(resp);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const recipe = await RecipeModel.findById(req.body.recipeID);
+    const user = await UserModel.findById(req.body.userID);
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.json({ savedRecipes: user.savedRecipes });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get("/savedRecipes/ids", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.userID);
+    res.json({ savedRecipes: user?.savedRecipes });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get("/savedRecipes", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.userID);
+    const savedRecipes = await RecipeModel.find({
+      _id: { $in: user.savedRecipes },
+    });
+    res.json({ savedRecipes });
   } catch (error) {
     console.error(error);
   }
--- client/src/pages/CreateRecipePage.js
+++ client/src/pages/CreateRecipePage.js
@@ -1,7 +1,102 @@
 import React from "react";
+import axios from "axios";
 
 const CreateRecipePage = () => {
-  return <div>CreateRecipeaPage</div>;
+  const [recipe, setRecipes] = React.useState({
+    name: "",
+    ingredients: [],
+    instructions: "",
+    imageUrl: "",
+    cookingTime: 0,
+    userOwner: 0,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRecipes({ ...recipe, [name]: value });
+  };
+
+  const handleIngChange = (e, idx) => {
+    const { value } = e.target;
+    const ingredients = recipe.ingredients;
+    ingredients[idx] = value;
+    setRecipes({ ...recipe, ingredients });
+  };
+
+  const addIngredient = () => {
+    setRecipes({
+      ...recipe,
+      ingredients: [...recipe.ingredients, ""],
+    });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/recipes", recipe);
+      alert("recipe created");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="create-recipe">
+      <h2>Create Recipe</h2>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={handleChange}
+        />
+
+        {/* <label htmlFor="description">Description</label>
+        <textarea name="description" id="description"></textarea> */}
+
+        <label htmlFor="ingredients">Ingredients</label>
+        {recipe.ingredients.map((ing, i) => (
+          <input
+            key={i}
+            type="text"
+            name="ingredients"
+            value={ing}
+            onChange={(e) => {
+              handleIngChange(e, i);
+            }}
+          />
+        ))}
+        <button type="submit" onClick={addIngredient}>
+          Add Ingredient
+        </button>
+
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          name="instructions"
+          id="instructions"
+          onChange={handleChange}
+        ></textarea>
+
+        <label htmlFor="imageUrl">Image Url</label>
+        <input
+          type="text"
+          id="imageUrl"
+          name="imageUrl"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="cookingTime">Image Url</label>
+        <input
+          type="number"
+          id="cookingTime"
+          name="cookingTime"
+          onChange={handleChange}
+        />
+        <button type="submit">Create Recipe</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateRecipePage;
